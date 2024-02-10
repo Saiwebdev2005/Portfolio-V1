933dddb48a11e3553d057ed8894dbbf999e137c9
saik434455@gmail.com <saik434455@gmail.com>
2024-02-10 11:07:53 +0530

    Skills section changed

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,26 +1,17 @@
-import React from 'react'
-import Card from './Card'
-import skillCardData from "./skillCardData"
-function page() {
+import SkillSection from "../../components/Skills/SkillSection";
+import skills from "../../components/Skills/skillCardData";
+function Page() {
   return (
-    <div className="bg-black h-screen w-full p-12">
-      <div className='flex flex-col justify-center items-center mx-auto md:max-w-3xl'>
-      <div className="grid grid-cols-3 lg:grid-cols-6 gap-8 md:gap-4">
-        {skillCardData.map((card, index) => (
-          <Card
-            key={index}
-            borderColor={card.borderColor}
-            gradientColors={card.gradientColors}
-            textColor={card.textColor}
-            shadowColor={card.shadowColor}
-            title={card.title}
-            content={card.content}
-          />
-        ))}
-      </div>
+    <div className="bg-black h-fit md:h-screen w-full p-12">
+      <div className="flex flex-col justify-center items-center space-y-6 mx-auto md:max-w-3xl ">
+        <SkillSection title="Web Development" skills={skills.webDev} />
+        <SkillSection
+          title="Blockchain Development"
+          skills={skills.blockChainDev}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
---
+++ src/components/Skills/SkillSection.jsx
@@ -0,0 +1,26 @@
+import Card from "../../app/test/Card";
+
+function SkillSection({ title, skills }) {
+  // Changed the prop name here
+  return (
+    <div className="flex flex-col justify-center items-center space-y-4">
+      <h1 className="text-4xl md:text-5xl text-center">{title}</h1>
+      <div className="flex flex-wrap  justify-center">
+        {skills.map((card, index) => (
+          <div className="m-4 md:m-2" key={index}>
+            <Card
+              borderColor={card.borderColor}
+              gradientColors={card.gradientColors}
+              textColor={card.textColor}
+              shadowColor={card.shadowColor}
+              imageURL={card.imageURL}
+              content={card.content}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SkillSection;
