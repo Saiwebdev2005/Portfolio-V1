9b2adbd7c34d9b2066d272b70fe974a79d11707a
saik434455@gmail.com <saik434455@gmail.com>
2024-02-29 18:21:25 +0530

    Hero Section Animated

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -7,27 +7,32 @@ const HeroSection = () => {
   let image = "/assets/astronaut.png";
   return (
     <div className="p-4 py-24 md:py-28 h-screen bg-c12 relative">
-      <Starfield className="absolute inset-0 z-0" />
+      {/* <Starfield className=" absolute inset-0 z-0" /> */}
       <div className="max-w-4xl mx-auto flex flex-col md:flex-row z-10 relative">
         {/* left section */}
         <div className="flex flex-col md:w-3/4 text-center md:items-left justify-center space-y-4 md:text-left">
-          <h2 className="text-white text-xl md:text-3xl font-semibold">
+          <h2 className="animate-slidein500 opacity-0 text-white text-xl md:text-3xl font-semibold">
             Hi, my name is
           </h2>
-          <HeadText />
-          <p className="text-2xl md:text-4xl font-semibold">
+          <div className="animate-slidein700 opacity-0">
+            <HeadText />
+          </div>
+
+          <p className="animate-slidein1000 opacity-0 text-2xl md:text-4xl font-semibold">
             Junior Web3 Developer
           </p>
         </div>
         {/* Right Section */}
-        <div className=" w-1/2 md:w-1/4 scale-125 md:scale-150 flex justify-center items-center mx-auto pt-6">
-          <img
-            src={image}
-            className="text-purple-600 floating  duration-100"
-            alt="Coding"
-            width="320"
-            height="420"
-          />
+        <div className=" w-1/2 md:w-1/4 scale-125 md:scale-150 flex justify-center items-center mx-auto pt-6 ">
+          <div className="animate-slidein700 opacity-0">
+            <img
+              src={image}
+              className=" text-purple-600 floating  duration-100"
+              alt="Coding"
+              width="320"
+              height="420"
+            />
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,25 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translatey(-30px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
         breath: {
           "0%": { "font-variation-settings": '"wght" 32' },
         },
         "100%": { "font-variation-settings": '"wght" 240' },
+      },
+      animation: {
+        slidein500: "slidein 1s ease 500ms forwards",
+        slidein700: "slidein 1s ease 700ms forwards",
+        slidein1000: "slidein 1s ease 1000ms forwards",
       },
       colors: {
         c1: "#000000",
