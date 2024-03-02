5f4725db37418db0d652ab97ef0c4f7341aca57d
saik434455@gmail.com <saik434455@gmail.com>
2024-03-02 22:01:16 +0530

    changed the svg in contact section

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,17 +1,38 @@
-import SkillSection from "../../components/Skills/SkillSection";
-import skills from "../../components/Skills/skillCardData";
-function Page() {
+"use client"
+import React, { useEffect, useState } from 'react';
+import style from "./grid.module.css"// Import the styles
+
+const TextAnimation = () => {
+  const [colorIndex, setColorIndex] = useState(0);
+
+  // Define your gradient colors
+  const gradientColors = [
+    '#4F46E5',
+    '#8D44AD',
+    '#D9526A',
+    '#E76F51',
+    '#F4D35E',
+  ];
+
+  useEffect(() => {
+    // Update the color index every 2 seconds
+    const interval = setInterval(() => {
+      setColorIndex((prevIndex) => (prevIndex + 1) % gradientColors.length);
+    }, 2000);
+
+    // Clear the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="bg-black h-fit md:h-screen w-full p-12">
-      <div className="flex flex-col justify-center items-center space-y-6 mx-auto md:max-w-3xl ">
-        <SkillSection title="Web Development" skills={skills.webDev} />
-        <SkillSection
-          title="Blockchain Development"
-          skills={skills.blockChainDev}
-        />
-      </div>
+    <div className={`text-4xl font-bold ${style.animate-gradient}`}>
+      <span
+        className={`bg-clip-text text-transparent bg-gradient-to-r from-${gradientColors[colorIndex]} to-${gradientColors[(colorIndex + 1) % gradientColors.length]}`}
+      >
+        Your Animated Text
+      </span>
     </div>
   );
-}
+};
 
-export default Page;
+export default TextAnimation;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,45 +1,60 @@
-import React from 'react';
-import ContactFormNew from './ContactForm';
-
+import React from "react";
+import ContactFormNew from "./ContactForm";
 
 // Data array
 const formFieldsData = [
-  { type: 'text', placeholder: 'Name' },
-  { type: 'text', placeholder: 'Email' },
-  { type: 'textarea', placeholder: 'Bio' },
+  { type: "text", placeholder: "Name" },
+  { type: "text", placeholder: "Email" },
+  { type: "textarea", placeholder: "Bio" },
 ];
 
 // Contact component
 const Contact = () => (
-  <div id="getInTouch" className='w-full h-fit p-12 my-36 bg-c2'>
-    <div className='relative container flex flex-col justify-center items-center mx-auto max-w-5xl space-y-8'>
+  <div id="getInTouch" className="w-full h-fit p-12 my-36 bg-c2">
+    <div className="relative container flex flex-col justify-center items-center mx-auto max-w-5xl space-y-8">
       {/* Animation Image */}
-      <div className='absolute left-0 top-0'>
-        <img src="/assets/asteroids1.png" alt="" className="w-12 h-12 floating" />
+      <div className="absolute -left-5 top-10">
+        <img src="/assets/solana.png" alt="" className="w-14 h-16 floating" />
       </div>
-      <div className='absolute -right-10 md:right-0 bottom-0'>
-        <img src="/assets/asteroids3.png" alt="" className="w-18 h-18 floating" />
+      <div className="absolute -right-10 md:right-0 bottom-0">
+        <img
+          src="/assets/bitcoin1.svg"
+          alt=""
+          className="w-20 h-22 md:h-24 floating"
+        />
       </div>
-      <div className='absolute -left-10 md:left-10 bottom-0'>
-        <img src="/assets/asteroids4.png" alt="" className="w-18 h-18 floating" />
+      <div className="absolute -left-10 md:left-10 bottom-0">
+        <img
+          src="/assets/bitcoin.svg"
+          alt=""
+          className="w-18 h-20 md:h-24 floating"
+        />
       </div>
-      <div className='absolute right-0 md:right-10 top-0'>
-        <img src="/assets/asteroids5.png" alt="" className="w-18 h-18 floating" />
+      <div className="absolute -right-10 md:right-10 top-0">
+        <img
+          src="/assets/polygon.svg"
+          alt=""
+          className="w-18 h-20 md:h-26 md:w-24 floating"
+        />
       </div>
-      <div className='flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-12'>
+      <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-12">
         {/* Left section */}
-        <div className='flex flex-col justify-center md:mb-16 items-center text-center md:text-left'>
-          <div className='w-64 space-y-4'>
-            <h1 className='text-6xl font-bold text-c3'>Get In Touch</h1>
-            <p className='font-poppins'>If you’re interested in working with me or if you have any questions, please don’t hesitate to reach out. I look forward to the opportunity to work with you and contribute to your success.</p>
+        <div className="flex flex-col justify-center md:mb-16 items-center text-center md:text-left">
+          <div className="w-64 space-y-4">
+            <h1 className="text-6xl font-bold text-c3">Get In Touch</h1>
+            <p className="font-poppins">
+              If you’re interested in working with me or if you have any
+              questions, please don’t hesitate to reach out. I look forward to
+              the opportunity to work with you and contribute to your success.
+            </p>
           </div>
         </div>
         {/* Right section */}
-        <div className='flex flex-col space-y-6 w-1/2 font-sans'>
+        <div className="flex flex-col space-y-6 w-1/2 font-sans">
           {/* {formFieldsData.map((field, index) => (
             <ContactForm key={index} {...field} />
           ))} */}
-          <ContactFormNew/>
+          <ContactFormNew />
         </div>
       </div>
     </div>
