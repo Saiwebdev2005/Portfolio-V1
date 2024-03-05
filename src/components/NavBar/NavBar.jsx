643c9d73275e62a93ca095e74d6aaa17c5571c3d
saik434455@gmail.com <saik434455@gmail.com>
2024-03-05 19:30:45 +0530

    Added New Logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,12 +6,12 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className=" p-2">
+    <nav className=" p-2 pt-4">
       <div className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-between">
             <div className="flex-shrink-0 flex items-center">
-              <h1 className="text-c2 text-2xl md:text-5xl">Sai</h1>
+              <img width="75px" height="75pxc:\Users\sasuk\OneDrive\Documents\favicon_io_prj_X\favicon.ico" src="/assets/Logo_Sai.png" />
             </div>
             {/* Desktop */}
             <div className="sm:ml-6">
