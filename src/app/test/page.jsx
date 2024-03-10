2ebb081ba8eae65075f8521cbf7b8bbd0f9b07aa
saik434455@gmail.com <saik434455@gmail.com>
2024-03-10 20:40:36 +0530

    Testing new head text

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,38 +1,11 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import style from "./grid.module.css"// Import the styles
-
-const TextAnimation = () => {
-  const [colorIndex, setColorIndex] = useState(0);
-
-  // Define your gradient colors
-  const gradientColors = [
-    '#4F46E5',
-    '#8D44AD',
-    '#D9526A',
-    '#E76F51',
-    '#F4D35E',
-  ];
-
-  useEffect(() => {
-    // Update the color index every 2 seconds
-    const interval = setInterval(() => {
-      setColorIndex((prevIndex) => (prevIndex + 1) % gradientColors.length);
-    }, 2000);
-
-    // Clear the interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
-
+import React from 'react'
+import HeadText from './HeadText'
+function page () {
   return (
-    <div className={`text-4xl font-bold ${style.animate-gradient}`}>
-      <span
-        className={`bg-clip-text text-transparent bg-gradient-to-r from-${gradientColors[colorIndex]} to-${gradientColors[(colorIndex + 1) % gradientColors.length]}`}
-      >
-        Your Animated Text
-      </span>
+    <div>
+<HeadText/>
     </div>
-  );
-};
+  )
+}
 
-export default TextAnimation;
+export default page
